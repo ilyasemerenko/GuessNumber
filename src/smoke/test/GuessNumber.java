020c56fc136d37aa65181abd35ae0ed36f2f01c9
Ilya Semerenko <Ilya.Semerenko@areon.ua>
2016-03-02 14:31:07 +0200

    extract method

--- src/smoke/test/GuessNumber.java
+++ src/smoke/test/GuessNumber.java
@@ -1,34 +1,12 @@
 package smoke.test;
 
-import java.util.Random;
-import java.util.Scanner;
-
 /**
  * Created by SemerenkoIlya on 29.02.2016.
  */
 public class GuessNumber {
     public static void main(String[] args){
-        Scanner myScanner = new Scanner(System.in);
-        Random rand = new Random();
         Utils util = new Utils();
-        boolean goOn = true;
+        util.run();
+    }
 
-    while (goOn) {
-        int randomNumber = rand.nextInt(20) + 1;
-        while (true) {
-            String userString = myScanner.nextLine();
-            if ("q".equalsIgnoreCase(userString)) {
-                System.out.println("Loser! Random number = " + randomNumber);
-                break;
-            } else if (randomNumber == Integer.valueOf(userString)) {
-                System.out.println("Got it! Let's try again!");
-                break;
-            } else if (randomNumber > Integer.valueOf(userString)) {
-                System.out.println("Go on. Your value is lower");
-            } else if (randomNumber < Integer.valueOf(userString)) {
-                System.out.println("Go on. Your value is higher");
-            }
-        }
-    }
-    }
 }
--- src/smoke/test/Utils.java
+++ src/smoke/test/Utils.java
@@ -1,5 +1,8 @@
 package smoke.test;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by SemerenkoIlya on 29.02.2016.
  */
@@ -17,5 +20,32 @@ public class Utils {
             return SAME;
         }
         return 3;
+    }
+
+    void run() {
+        Scanner myScanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean goOn = true;
+        try {
+            while (goOn) {
+                int randomNumber = random.nextInt(20) + 1;
+                while (true) {
+                    String userString = myScanner.nextLine();
+                    if ("q".equalsIgnoreCase(userString)) {
+                        System.out.println("Loser! Random number = " + randomNumber);
+                        break;
+                    } else if (randomNumber == Integer.valueOf(userString)) {
+                        System.out.println("Got it! Let's try again!");
+                        break;
+                    } else if (randomNumber > Integer.valueOf(userString)) {
+                        System.out.println("Go on. Your value is lower");
+                    } else if (randomNumber < Integer.valueOf(userString)) {
+                        System.out.println("Go on. Your value is higher");
+                    }
+                }
+            }
+        } catch (Exception e){
+            System.out.println("Exception + " + e);
+        }
     }
 }
